e8c2e155923dfc0b08bfa93b8b9d813302e2ad42
NicolasChuquimantari <nicochuqui10@gmail.com>
2026-05-29 22:14:59 +0000

    Fri, May 29, 2026, 7:14 PM -03:00

--- week1/meal/meal.py
+++ week1/meal/meal.py
@@ -1,13 +1,12 @@
 def main():
     time : input("What time is it? ")
     convert_time = convert(time)
-
-if 7 <= convert_time <= 8:
-    print("breakfast time")
-elif 12 <= convert_time <= 13:
-    print("lunch time")
-elif 18 <= convert_time <= 19:
-    print("dinner time")
+    if 7 <= convert_time <= 8:
+        print("breakfast time")
+    elif 12 <= convert_time <= 13:
+        print("lunch time")
+    elif 18 <= convert_time <= 19:
+        print("dinner time")
 
 def convert(time):
     h, m = time.split(":")
