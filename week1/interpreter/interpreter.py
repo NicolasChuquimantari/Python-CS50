1147af02f90c04df539fbe888a1e9467a7f4a216
NicolasChuquimantari <nicochuqui10@gmail.com>
2026-05-29 21:21:43 +0000

    Fri, May 29, 2026, 6:21 PM -03:00

--- week1/interpreter/interpreter.py
+++ week1/interpreter/interpreter.py
@@ -1,4 +1,14 @@
 expression = ("Enter arithmetic expression: ")
 
 x, y, z = expression.split(" ")
+x = float(x)
+z = float(z)
 
+if y == "+":
+    print(x+z)
+elif y == "-":
+    print(x-z)
+elif y == "*":
+    print(x*z)
+else:
+    print(x/z)
