0c6315cc0f36ccfff56407a30b64b344bfddaa7c
NicolasChuquimantari <nicochuqui10@gmail.com>
2026-06-03 20:49:46 +0000

    Wed, Jun 3, 2026, 5:49 PM -03:00

--- week2/nutrition/nutrition.py
+++ week2/nutrition/nutrition.py
@@ -24,5 +24,5 @@ fruits_calories:{
 fruit = input("Enter the name of the fruit: ").lower()
 
 if fruit in fruits_calories:
-    print(f"Calories: fruits_calories[fruit])
+    print(f"Calories: fruits_calories[fruit]")
 
