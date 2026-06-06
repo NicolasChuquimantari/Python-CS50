9ade126f0285274d87c2a35d6267a7d1d24840d7
NicolasChuquimantari <nicochuqui10@gmail.com>
2026-06-06 22:52:22 +0000

    Sat, Jun 6, 2026, 7:52 PM -03:00

--- week3/outdated/outdated.py
+++ week3/outdated/outdated.py
@@ -20,7 +20,9 @@ while True:
             date_format = date.split("/")
             month, day, year = int(date_format)
             if 1 <= month <= 12 and 1 <= day <= 31:
-                print(f")
+                print(f"{year}-{month:02}-{day:02}")
+
+
 
 
 
