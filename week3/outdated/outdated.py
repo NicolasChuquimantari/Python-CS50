a61fd528b194ba09def5dce7d9d6c90eda0c9125
NicolasChuquimantari <nicochuqui10@gmail.com>
2026-06-06 21:37:02 +0000

    Sat, Jun 6, 2026, 6:37 PM -03:00

---
+++ week3/outdated/outdated.py
@@ -0,0 +1,16 @@
+list = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+
