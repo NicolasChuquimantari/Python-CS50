796bc1f9794ade50a51c111cda6fcbc96892caa6
NicolasChuquimantari <nicochuqui10@gmail.com>
2026-06-16 13:39:37 +0000

    Tue, Jun 16, 2026, 10:39 AM -03:00

--- week4/figlet/figlet.py
+++ week4/figlet/figlet.py
@@ -10,7 +10,10 @@ if len(sys.argv) == 1:
     font = random.choice(fonts)
     fig.setFont(font=font)
 
-elif len(sys.argv) == 3
+elif len(sys.argv) == 3:
+    text = input("Enter text: ")
+    if 
+
 
 
 
