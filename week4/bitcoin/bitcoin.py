62f5860276704099f295c4aebbaf0829a08ab2fb
NicolasChuquimantari <nicochuqui10@gmail.com>
2026-06-19 14:19:37 +0000

    Fri, Jun 19, 2026, 11:19 AM -03:00

--- week4/bitcoin/bitcoin.py
+++ week4/bitcoin/bitcoin.py
@@ -5,9 +5,13 @@ import sys
 if len(sys.argv) == 2:
     try:
         bitcoin_n = float(sys.argv[1])
+    except ValueError:
+        sys.exit("Command-line argument is not a number")
 
 
-if len(sys.argv) != 2:
+elif len(sys.argv) != 2:
     sys.exit("Missing command-line argument")
+
+    
 
 
