cd1e1d9b43fb0614e0ad49010f82536c9eb86e67
NicolasChuquimantari <nicochuqui10@gmail.com>
2026-06-23 21:21:14 +0000

    Tue, Jun 23, 2026, 6:21 PM -03:00

--- week5/test_twttr/twttr.py
+++ week5/test_twttr/twttr.py
@@ -10,7 +10,7 @@ def shorten(word):
     n_word = []
     for char in word:
         if char not in vowels:
-            n_word += char
+            n_word.append(char)
 
     return "".join(n_word)
 
