81ca3a73eae1670f74ed47ce37575a17d925877e
NicolasChuquimantari <nicochuqui10@gmail.com>
2026-07-01 19:58:12 +0000

    Wed, Jul 1, 2026, 4:58 PM -03:00

--- week5/test_plates/test_plates.py
+++ week5/test_plates/test_plates.py
@@ -2,7 +2,7 @@ from plates import is_valid
 
 def test_length():
     assert is_valid("Hello") == True
-    assert is_valid("H") == False
+    assert is_valid("Good morning") == False
     assert is_valid("Hi") == True
 
 def test_starts_alpha():
