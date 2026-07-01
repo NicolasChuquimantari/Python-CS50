3014958b38b67f2414aec9486a472b036e184964
NicolasChuquimantari <nicochuqui10@gmail.com>
2026-07-01 13:32:52 +0000

    Wed, Jul 1, 2026, 10:32 AM -03:00

--- week5/test_bank/test_bank.py
+++ week5/test_bank/test_bank.py
@@ -2,4 +2,11 @@ from bank import value
 
 def test_hello():
     assert value("Hello") == 0
-    assert value(")
+    assert value("HELLO") == 0
+
+def test_h():
+    assert value("Hi") == 20
+    assert value("HI") == 20
+
+def test_else()
+
