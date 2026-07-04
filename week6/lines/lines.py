233acbdea6479658791751694b0821534226206f
NicolasChuquimantari <nicochuqui10@gmail.com>
2026-07-04 15:52:26 +0000

    Sat, Jul 4, 2026, 12:52 PM -03:00

--- week6/lines/lines.py
+++ week6/lines/lines.py
@@ -1,12 +1,12 @@
 import sys
 
 
-if not sys.argv[1].endswith(".py"):
-    sys.exit("Not a Python file")
-elif len(sys.argv) < 2:
+if len(sys.argv) < 2:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 2:
     sys.exit("Too many command-line arguments")
+if not sys.argv[1].endswith(".py"):
+    sys.exit("Not a Python file")
 
 try:
     with open(sys.argv[1]) as file:
