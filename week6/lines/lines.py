e53b2e7352c16fdbb6c62634dd34f6a8c43eb84f
NicolasChuquimantari <nicochuqui10@gmail.com>
2026-07-04 15:49:21 +0000

    Sat, Jul 4, 2026, 12:49 PM -03:00

--- week6/lines/lines.py
+++ week6/lines/lines.py
@@ -12,7 +12,7 @@ try:
     with open(sys.argv[1]) as file:
         count_line = 0
         for line in file:
-            if not (line.lstrip().startswith("#") or line.lstrip()):
+            if line.strip() and not line.lstrip().startswith("#"):
                  count_line +=1
 
     print(count_line)
