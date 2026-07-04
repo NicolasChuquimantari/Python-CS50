08e53c71922ac0626cd81267efa294ddc7dba059
NicolasChuquimantari <nicochuqui10@gmail.com>
2026-07-04 15:12:59 +0000

    Sat, Jul 4, 2026, 12:12 PM -03:00

--- week6/lines/lines.py
+++ week6/lines/lines.py
@@ -1,8 +1,12 @@
 import sys
 
-if len(sys.argv) < 2:
+
+if not sys.argv[1].endswith(".py"):
+    sys.exit("Not a Python file")
+elif len(sys.argv) < 2:
     sys.exit("Too few command-line arguments")
-elif len(sys.argv) > 2:
+else len(sys.argv) > 2:
     sys.exit("Too many command-line arguments")
 
+try 
 
