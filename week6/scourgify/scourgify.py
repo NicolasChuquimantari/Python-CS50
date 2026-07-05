ec035bb86a8a05383e9d0c6d7d774866ee1bee9a
NicolasChuquimantari <nicochuqui10@gmail.com>
2026-07-05 19:40:53 +0000

    Sun, Jul 5, 2026, 4:40 PM -03:00

--- week6/scourgify/scourgify.py
+++ week6/scourgify/scourgify.py
@@ -6,4 +6,15 @@ if len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
-    
+
+try:
+    with open(sys.argv[1]) as infile:
+
+
+
+
+
+
+
+except FileNotFoundError:
+    sys.exit("Could not read invalid_file.csv")
