9e7696abc71534e1dc5b857a415e535ee8e297c8
NicolasChuquimantari <nicochuqui10@gmail.com>
2026-07-07 19:34:39 +0000

    Tue, Jul 7, 2026, 4:34 PM -03:00

--- week6/shirt/shirt.py
+++ week6/shirt/shirt.py
@@ -1,5 +1,4 @@
 import sys
-import os
 from PIL import Image, ImageOps
 
 if len(sys.argv) < 3:
@@ -10,9 +9,9 @@ elif len(sys.argv) > 3:
 if sys.argv[1].split(".")[1] != sys.argv[2].split(".")[1]:
     sys.exit("Input and output have different extensions")
 
-if not sys.argv[1].lower().endswith((".jpg", ".jpeg", ".png")) or not sys.argv[2].lower().endswith((".jpg", ".jpeg", ".png")):
+if not sys.argv[1].endswith((".jpg", ".jpeg", ".png")) or not sys.argv[2].endswith((".jpg", ".jpeg", ".png")):
         sys.exit("Invalid output")
 
-
+try
 
 
