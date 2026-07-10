1a9021b9fee65a1646794c07e4c1c3a091aef5d2
NicolasChuquimantari <nicochuqui10@gmail.com>
2026-07-10 15:38:29 +0000

    Fri, Jul 10, 2026, 12:38 PM -03:00

--- week7/working/working.py
+++ week7/working/working.py
@@ -8,7 +8,14 @@ def main():
 
 def convert(s):
     if hour := re.search(r"([0-9]{1,2})((:[0-9]{2})?) AM to ([0-9]{1,2})((:[0-9]{2})?) PM"):
-        
+
+#split into groups
+        hour1 = hour.group(1)
+        min1 = hour.group(2)
+
+        hour2 = hour.group(3)
+        min2 = hour.group(4)
+
 
 
 
