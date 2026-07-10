05efe49c41bcefe267ea96d617454d6ccd61622c
NicolasChuquimantari <nicochuqui10@gmail.com>
2026-07-10 14:16:15 +0000

    Fri, Jul 10, 2026, 11:16 AM -03:00

--- week7/watch/watch.py
+++ week7/watch/watch.py
@@ -7,11 +7,8 @@ def main():
 
 
 def parse(s):
-    ...
-
-
-...
-
+    if web_link := re.search(r'<iframe.+?src="https?://(www\.)?youtube\.com/embed/(\w+)"', s):
+        return f"https://youtu.be/{web_link.group(2)}"
 
 if __name__ == "__main__":
     main()
